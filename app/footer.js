514516a70c2b3fa2f4bf9b94d7b974dabd3af4c3
Sahil saw <sahilsaw23@gmail.com>
2025-05-23 16:52:42 +0530

    final project

---
+++ app/footer.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+export default function Footer() {
+  const year = new Date().getFullYear();
+  
+  return (
+    <footer className="h-16 flex justify-center items-center gap-6">
+      <p>© {year} Group Project</p>
+      <div className="hidden sm:flex text-sm items-center  gap-2 bg-black border border-neutral-700 text-white px-4 py-2 rounded-lg">
+        Built with{" "}
+        <Image
+          className="invert h-3 w-auto"
+          src="/next.svg"
+          alt="NextJS Logo"
+          width={72}
+          height={16}
+        />
+      </div>
+    </footer>
+  );
+}
